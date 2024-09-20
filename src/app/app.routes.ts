fd30da5c6026aab065e35e8d5dfc9b39aaba27af
CristinaOsorio <macry_08@hotmail.com>
2024-09-20 12:06:22 -0600

    feat: add contact-list component and set it as the default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contacts',
+        loadComponent: () => import('./features/contacts/contact-list/contact-list.component')
+
+    },
+    {
+        path: '**',
+        redirectTo: '/contacts',
+        pathMatch: 'full'
+    }
+];
