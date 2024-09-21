0b62bfa56f84603bafdc0c211c939b75098fac0e
CristinaOsorio <macry_08@hotmail.com>
2024-09-21 10:03:45 -0600

    feat: connect contact service to API

--- src/app/core/interfaces/paginate.interface.ts
+++ src/app/core/interfaces/paginate.interface.ts
@@ -1,3 +1,9 @@
 export interface PaginatedResponse<T> {
     data: T[];
+    current_page: number;
+    from: number;
+    last_page: number;
+    per_page: number;
+    to: number;
+    total: number;
 }
